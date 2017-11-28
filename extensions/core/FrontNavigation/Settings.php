4ab2c70fe003ca5aadfde78653fa4d8c21ca1a7b
Michael White <mike.wildcats@gmail.com>
2017-11-28 16:30:43 -0500

    Settings active url

---
+++ extensions/core/FrontNavigation/Settings.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * @brief		Front Navigation Extension: Settings
+ * @author		<a href='https://www.invisioncommunity.com'>Invision Power Services, Inc.</a>
+ * @copyright	(c) Invision Power Services, Inc.
+ * @license		https://www.invisioncommunity.com/legal/standards/
+ * @package		Invision Community
+ * @subpackage	Gamepanel
+ * @since		28 Nov 2017
+ */
+
+namespace IPS\gamepanel\extensions\core\FrontNavigation;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Front Navigation Extension: Settings
+ */
+class _Settings extends \IPS\core\FrontNavigation\FrontNavigationAbstract
+{
+	/**
+	 * Get Type Title which will display in the AdminCP Menu Manager
+	 *
+	 * @return	string
+	 */
+	public static function typeTitle()
+	{
+		return \IPS\Member::loggedIn()->language()->addToStack('module__gamepanel_settings');
+	}
+	
+	/**
+	 * Can this item be used at all?
+	 * For example, if this will link to a particular feature which has been diabled, it should
+	 * not be available, even if the user has permission
+	 *
+	 * @return	bool
+	 */
+	public static function isEnabled()
+	{
+		return TRUE;
+	}
+	
+	/**
+	 * Can the currently logged in user access the content this item links to?
+	 *
+	 * @return	bool
+	 */
+	public function canAccessContent()
+	{
+		return TRUE;
+	}
+	
+	/**
+	 * Get Title
+	 *
+	 * @return	string
+	 */
+	public function title()
+	{
+		return \IPS\Member::loggedIn()->language()->addToStack('module__gamepanel_settings');
+	}
+	
+	/**
+	 * Get Link
+	 *
+	 * @return	\IPS\Http\Url
+	 */
+	public function link()
+	{
+		return \IPS\Http\Url::internal( "app=gamepanel&module=gamepanel&controller=settings" );
+	}
+	
+	/**
+	 * Is Active?
+	 *
+	 * @return	bool
+	 */
+	public function active()
+	{
+		return \IPS\Dispatcher::i()->application->directory === 'gamepanel' and \IPS\Dispatcher::i()->module->key === 'settings';
+	}
+
+	/**
+	 * Children
+	 *
+	 * @param	bool	$noStore	If true, will skip datastore and get from DB (used for ACP preview)
+	 * @return	array
+	 */
+	public function children( $noStore=FALSE )
+	{
+		return NULL;
+	}
+}
